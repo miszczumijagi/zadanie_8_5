3c8bfcca2bd2fde9a783672f3813a4640156e175
Kamil Stangret <kamilstangret@wp.pl>
2017-12-22 21:53:27 +0100

    Add scripts.js with content

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//scripts.js
+
+var a = prompt('Enter the base of the triangle'),
+	h = prompt('Enter the height of the triangle'),
+	triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
